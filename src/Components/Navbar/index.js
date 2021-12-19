b65b57dda07d16c77e2e2ca45d954d30b7168bde
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2021-12-19 21:45:46 +0900

    Installed new React-dom packages and made a new Navbar

---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import { Nav, NavLink, Bars, NavMenu } from "./NavbarElements";
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLink to="/">
+          <h1>Logo</h1>
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to="/about" activeStyle>
+            About Me
+          </NavLink>
+          <NavLink to="/my-projects" activeStyle>
+            My Projects
+          </NavLink>
+          <NavLink to="/contact-me" activeStyle>
+            Contact Me
+          </NavLink>
+          <NavLink to="/services" activeStyle>
+            Services
+          </NavLink>
+        </NavMenu>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
