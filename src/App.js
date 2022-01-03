269705e41d2dce01fab2eda0d7af9d855aff6133
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-01-03 19:21:21 +0900

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages";
+import MyWork from "./Pages/MyWork";
+import Footer from "./Footer";
 
 export default function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/MyWork" element={<MyWork />} />
+        </Routes>
+      </Router>
+
+      <Footer />
+    </div>
   );
 }
